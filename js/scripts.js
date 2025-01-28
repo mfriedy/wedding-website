c4507938790ba27195259594f0f912858c847f5a
Matthew Fried <matt.n.fried@gmail.com>
2025-01-28 21:45:21 +1100

    More js toggling

--- js/scripts.js
+++ js/scripts.js
@@ -13,19 +13,23 @@ document.getElementById('rsvp-password-form').addEventListener('submit', functio
         .then(response => response.json())
         .then(data => {
             if (data.success) {
-                document.getElementById('rsvpUnlockModalToggle').hide();
+                var myModalEl = document.getElementById('rsvpUnlockModalToggle');
+                var modal = bootstrap.Modal.getInstance(myModalEl)
+                modal.hide();
                 document.getElementById('form_iframe').src = data.form_url;
-                document.getElementById('rsvpFormModalToggle').show();
+                var myModelR = document.getElementById('rsvpFormModalToggle');
+                var modalR = bootstrap.Modal.getInstance(myModelR)
+                modalR.show();
 
             } else {
                 document.getElementById('password-error').style.display = 'block';
+                const passwordError = document.createElement('div');
+                passwordError.id = 'password-error';
+                passwordError.className = 'text-danger';
+                passwordError.style.display = 'none';
+                passwordError.textContent = 'Incorrect password. Please try again.';
+                document.getElementById('password-form').appendChild(passwordError);
             }
-            const passwordError = document.createElement('div');
-            passwordError.id = 'password-error';
-            passwordError.className = 'text-danger';
-            passwordError.style.display = 'none';
-            passwordError.textContent = 'Incorrect password. Please try again.';
-            document.getElementById('password-form').appendChild(passwordError);
         })
         .catch(error => {
             console.error('Error:', error);
